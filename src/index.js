d09a3b724754f8f6249da5063f8573124182fff4
psurge1 <suraj.sswamy@gmail.com>
2022-04-12 11:59:18 -0500

    init

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var element = React.createElement('h1', {className:'greeting'}, 'HelloWorld!')
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
